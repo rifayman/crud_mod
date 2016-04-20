ad603b8dc6d8e654f12a49779c01312093f86283
Eric Lagarda <eric@infinety.es>
2016-04-20 12:51:11 +0200

    Browse add-on updated

--- src/resources/views/fields/assets/js/browse.blade.php
+++ src/resources/views/fields/assets/js/browse.blade.php
@@ -3,11 +3,12 @@
 
     jQuery(document).ready(function($) {
         uploadFile = function(dat){
+            var id = dat.replace('-file', '');
             $.fancybox({
                 width		: 950,
                 height	    : 500,
                 type		: 'iframe',
-                href        : "http://demo.starter.on/admin/filemanager/dialog?type=featured",
+                href        : "{{ url('admin/filemanager/dialog') }}?type=featured&appendId="+id,
                 fitToView   : false,
                 autoScale   : false,
                 autoSize    : false
@@ -19,8 +20,9 @@
             console.log(data);
             var patt = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif|bmp))/i;
             if(patt.test(data.thumb)){
-                $("#{{ $field['name'] }}-text").val(data.thumb);
-                $("#{{ $field['name'] }}").attr({ 'src' : data.thumb});
+                $("#"+data.appendId+"-text").val(data.thumb);
+                $("#"+data.appendId).attr({ 'src' : data.thumb});
+                $("#"+data.appendId).removeClass('hide');
                 $.fancybox.close();
             }
         };
--- src/resources/views/fields/browse.blade.php
+++ src/resources/views/fields/browse.blade.php
@@ -1,19 +1,19 @@
 <!-- browse server input -->
-	<img class="output" src="" width="20%" height="5%" id="{{ $field['name'] }}">
-  <div class="form-group form-group-default input-group">
-    <label>{{ $field['label'] }}</label>
+	<img class="output hide" src="" width="20%" height="5%" id="{{ $field['name'] }}">
+	<div class="form-group form-group-default input-group">
+		<label>{{ $field['label'] }}</label>
 
-	<input type="input" class="form-control" id="{{ $field['name'] }}-text"
-		@foreach ($field as $attribute => $value)
-			@if($attribute != "name")
-			{{ $attribute }}="{{ $value }}"
-		  	@endif
-		@endforeach
-	>
-	<span class="input-group-addon bg-transparent">
-		<button type="button" data-inputid="{{ $field['name'] }}-file" class="btn btn-default popup_selector file_click" onclick="uploadFile('{{ $field['name'] }}-file');">
-			<i class="fa fa-cloud-upload"></i> Browse uploads
-		</button>
-	</span>
+		<input type="input" class="form-control" id="{{ $field['name'] }}-text"
+			@foreach ($field as $attribute => $value)
+				@if($attribute != "name")
+				{{ $attribute }}="{{ $value }}"
+				@endif
+			@endforeach
+		>
+		<span class="input-group-addon bg-transparent">
+			<button type="button" data-inputid="{{ $field['name'] }}-file" class="btn btn-default popup_selector file_click" onclick="uploadFile('{{ $field['name'] }}-file');">
+				<i class="fa fa-cloud-upload"></i> Browse uploads
+			</button>
+		</span>
 
-  </div>
+	</div>
