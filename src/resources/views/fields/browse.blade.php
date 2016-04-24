e20cfb9e82e25252932421880856c45cfc3de7b9
Eric Lagarda <info@ericlagarda.com>
2016-04-24 09:47:30 +0200

    Rose field name fix and Preview

--- src/resources/views/fields/browse.blade.php
+++ src/resources/views/fields/browse.blade.php
@@ -1,13 +1,11 @@
-<!-- browse server input -->
-	<img class="output hide" src="" width="20%" height="5%" id="{{ $field['name'] }}">
+
+	<img class="output {{ (isset($field["value"]) &&  !empty($field["value"])) ? '' : 'hide' }}" src="{{ (isset($field["value"]) &&  !empty($field["value"])) ? $field["value"] : '' }}" width="20%" height="5%" id="{{ $field['name'] }}">
 	<div class="form-group form-group-default input-group">
 		<label>{{ $field['label'] }}</label>
 
 		<input type="input" class="form-control" id="{{ $field['name'] }}-text"
 			@foreach ($field as $attribute => $value)
-				@if($attribute != "name")
 				{{ $attribute }}="{{ $value }}"
-				@endif
 			@endforeach
 		>
 		<span class="input-group-addon bg-transparent">
