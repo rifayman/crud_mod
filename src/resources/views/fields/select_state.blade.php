ed6bf4b8c92d13a92c3f60f89fc7598931d27d31
Eric Lagarda <eric@infinety.es>
2016-04-26 15:18:04 +0200

    Select state fix

--- src/resources/views/fields/select_state.blade.php
+++ src/resources/views/fields/select_state.blade.php
@@ -1,13 +1,17 @@
-<!-- select -->
-  <div class="form-group">
-
-    <select
-            name="{{$field['name']}}"
-            class="form-control"
-            id="select_state">
-
-            <option value="0" {{ ($field['value'] == 0) ? 'selected' : '' }}>Disabled</option>
-
-	       <option value="1" {{ ($field['value'] == 1)  ? 'selected' : '' }}>Active</option>
+<div class="form-group">
+    <?php
+    $value = 0;
+    if(isset($field['defaultValue'])){
+        $value = $field['defaultValue'];
+    }
+    if(isset($field['value'])){
+        if($field['value'] == 1){
+            $value = 1;
+        }
+    }
+    ?>
+    <select name="{{$field['name']}}" class="form-control" id="select_state_{{ $field["name"] }}">
+            <option value="0" {{ ($value == 0) ? 'selected' : '' }}>Disabled</option>
+            <option value="1" {{ ($value == 1)  ? 'selected' : '' }}>Active</option>
 	</select>
-  </div>
+</div>
