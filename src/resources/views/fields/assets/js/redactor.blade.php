de1ed9c6ac3fb102219a0fe029ecbd91c7db7279
Eric Lagarda <eric@infinety.es>
2016-06-30 13:03:57 +0200

    FIX  redactor field for non translated fields

--- src/resources/views/fields/assets/js/redactor.blade.php
+++ src/resources/views/fields/assets/js/redactor.blade.php
@@ -5,24 +5,42 @@
 <script src="{{ asset('/admin_theme/assets/plugins/redactor/plugins/imagemanager.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/admin_theme/assets/plugins/redactor/plugins/video.js') }}" type="text/javascript"></script>
 <script>
+	@if(isset($language))
+		@foreach($language as $localeCode => $properties)
+		//		$('.redactor').redactor();
 
-	@foreach($language as $localeCode => $properties)
-	//		$('.redactor').redactor();
-	jQuery(document).ready(function($) {
-		$('#redactor-{{$properties["iso"]}}').redactor({
-			minHeight: 350,
-			maxHeight: 800,
-			cleanOnPaste: true,
-			cleanSpaces: true,
-			removeComments: true,
-			removeEmpty: ['strong', 'em', 'span', 'p'],
-			buttonsHide: ['orderedlist', 'image'],
-			formatting: ['p', 'blockquote', 'h2', 'h3', 'h4'],
-			plugins: ['fullscreen',  'video', 'imagemanager'],
-			imageManagerUrl: "{{ url('admin/filemanager/dialog') }}?type=editor&editor=redactor-{{$properties["iso"]}}"
+		jQuery(document).ready(function($) {
+			$('#redactor-{{$field["name"]}}-{{$properties["iso"]}}').redactor({
+				minHeight: 350,
+				maxHeight: 800,
+				cleanOnPaste: true,
+				cleanSpaces: true,
+				removeComments: true,
+				removeEmpty: ['strong', 'em', 'span', 'p'],
+				buttonsHide: ['orderedlist', 'image'],
+				formatting: ['p', 'blockquote', 'h2', 'h3', 'h4'],
+				plugins: ['fullscreen',  'video', 'imagemanager'],
+				imageManagerUrl: "{{ url('admin/filemanager/dialog') }}?type=editor&editor=redactor-{{$field["name"]}}-{{$properties["iso"]}}"
+			});
 		});
-	});
 
-	@endforeach
+		@endforeach
+	@else
 
+		jQuery(document).ready(function($) {
+			$('#redactor-{{$field["name"]}}').redactor({
+				minHeight: 350,
+				maxHeight: 800,
+				cleanOnPaste: true,
+				cleanSpaces: true,
+				removeComments: true,
+				removeEmpty: ['strong', 'em', 'span', 'p'],
+				buttonsHide: ['orderedlist', 'image'],
+				formatting: ['p', 'blockquote', 'h2', 'h3', 'h4'],
+				plugins: ['fullscreen',  'video', 'imagemanager'],
+				imageManagerUrl: "{{ url('admin/filemanager/dialog') }}?type=editor&editor=redactor-{{$field["name"]}}"
+			});
+		});
+
+	@endif
 </script>
